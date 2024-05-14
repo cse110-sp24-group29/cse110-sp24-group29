1670933b429bcca0d2e030c5510d449142f5bbac
Saksham Rai <s1rai@ucsd.edu>
2024-05-14 07:17:04 -0700

    Update sample.js

--- source/script/sample.js
+++ source/script/sample.js
@@ -1,1 +1,19 @@
 
+/**
+ * Represents a book.
+ * @constructor
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ */
+function Book(title, author) {
+    this.title = title;
+    this.author = author;
+}
+
+/**
+ * Get the book description.
+ * @return {string} Description of the book.
+ */
+Book.prototype.getDescription = function() {
+    return `${this.title} by ${this.author}`;
+};
