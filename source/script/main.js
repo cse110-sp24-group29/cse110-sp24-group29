ea2da349f5cfc611d19bb058e14f20e64d92481a
Saksham Rai <raisaksham2001@gmail.com>
2024-05-22 22:41:35 -0700

    Update main.js

--- source/script/main.js
+++ source/script/main.js
@@ -1,14 +1,36 @@
+/**
+ * Get the container element from the DOM.
+ */
 const boxContainerElement = document.getElementById('box-container');
+
+/**
+ * Get the register button element from the DOM.
+ */
 const signUpButtonElement = document.getElementById('register');
+
+/**
+ * Get the login button element from the DOM.
+ */
 const signInButtonElement = document.getElementById('login');
 
+/**
+ * Handles the event when the sign-up button is clicked.
+ * Adds 'active' class to the box container element to show the sign-up form.
+ */
 function handleSignUpButtonClick() {
     boxContainerElement.classList.add('active');
 }
 
+/**
+ * Handles the event when the sign-in button is clicked.
+ * Removes 'active' class from the box container element to hide the sign-up form.
+ */
 function handleSignInButtonClick() {
     boxContainerElement.classList.remove('active');
 }
 
+/**
+ * Adds event listeners to sign-up and sign-in buttons.
+ */
 signUpButtonElement.addEventListener('click', handleSignUpButtonClick);
 signInButtonElement.addEventListener('click', handleSignInButtonClick);
