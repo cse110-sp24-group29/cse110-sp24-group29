c1905954093e104d58662f927b58dde1e7d09476
rli128 <114562309+rli128@users.noreply.github.com>
2024-06-09 21:21:39 -0700

    added logout button testing as well as home button testing

--- __tests__/ProjectPage.test.js
+++ __tests__/ProjectPage.test.js
@@ -245,14 +245,25 @@ test('notepad textbox changes when markdown button is clicked', async () => {
 }, 10000);
 
 
-// test('log out button goes to sign-in page', async () => {
-//   await page.waitForSelector('.logout-button');
-//   await page.hover('.logout-button');
-//   await page.click('.logout-button');
+test('log out button goes to sign-in page', async () => {
+  await page.waitForSelector('.logout-button');
+  await page.hover('.logout-button');
+  await page.click('.logout-button');
   
-//   const url = page.url();
-//   expect(url).toContain('index.html');
-// });
+  const url = page.url();
+  expect(url).toContain('index.html');
+});
+
+test('home button goes to home page', async () => {
+  await page.goto('http://localhost:3000/project.html');
+  await page.waitForSelector('.home-button');
+  await page.hover('.home-button');
+  await page.click('.home-button');
+  
+  const url = page.url();
+  expect(url).toContain('home.html');
+});
+
 
 test('should render markdown note with clickable link', async () => {
   await page.goto('http://localhost:3000/project.html');
